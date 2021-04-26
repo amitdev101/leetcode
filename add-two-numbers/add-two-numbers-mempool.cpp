1ace902ca7e2591ee81d48cfb0738602d48e2975
Amit <fulldev101@gmail.com>
2021-04-26 20:34:01 +0530

    added two numbers solved

---
+++ add-two-numbers/add-two-numbers-mempool.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode nodepool[200];
+    int nodepoolindex = 0;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // using carry method
+        ListNode* head = nullptr;
+        ListNode* tail = nullptr;
+        ListNode* tempnode;
+        int carry=0, r=0;
+        int num1,num2;
+        while(carry || l1 || l2){
+            num1 = num2 = 0;
+            if(l1){
+                num1 = l1->val;
+                l1 = l1->next;
+            }
+            if (l2){
+                num2 = l2->val;
+                l2 = l2->next;
+            }
+            
+            int num = num1 + num2 + carry;
+            carry = num/10;
+            r = num%10;
+            // tempnode = new ListNode(r);
+            tempnode = mynew(r);
+            if(!head){
+                head = tail = tempnode;
+            }
+            else {
+                tail->next = tempnode;
+                tail = tempnode;
+            }
+            
+        }
+        return head;
+    }
+
+    ListNode* mynew(int num){
+        ListNode* temp = &nodepool[nodepoolindex++];
+        temp->val = num;
+        temp->next = nullptr;
+        return temp;
+    }
+
+};

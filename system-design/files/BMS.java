d836dee1ea939d4a42610335b2cffb2a91e122a2
Amit <fulldev101@gmail.com>
2022-10-02 21:45:41 +0530

    design lld refactored

--- system-design/files/BMS.java
+++ system-design/files/BMS.java
@@ -1,81 +1,60 @@
 public class BMSService  {
-
 	List<CinemaHall> cinemas;
-
 	public List<Movie> getMovies(Date date, String city);
 	public List<CinemaHall> getCinemalHalls(String city);
 
 }
 
 public class CinemaHall {
-
 	int cinemaHallId;
 	String cinemaHallName;
-
 	Address address;
-
 	List<Audi>audiList;
-
 	public Map<Date, Movies> getMovies(List<Date> dateList);
 	public Map<Date, Show> getShows(List<Date> dateList);
-
 }
 
 public class Address {
-
 	int pinCode; //ZipCode
 	String street;
 	String city;
 	String state;
 	String country;
-
 }
 
 public class Audi {
-
 	int audiId;
 	String audiName;
 	int totalSeats;
-	
 	List<Show> shows;
-
 }
 
 public class Show {
-
 	int showId;
 	Movie movie;
 	Date startTime;
 	Date endTime;
 	CinemaHall cinemaPlayedAt;
 	List<Seat> seats;
-
 }
 
 public class Seat {
-
 	int seatId;
 	SeatType seatType;
 	SeatStatus seatStatus;
 	Double price;
-
 }
 
 
 public enum SeatType {
-
 	DELUX, VIP, ECONOMY, OTHER;
-
 }
 
 public enum SeatStatus {
-
 	BOOKED, AVAILABLE, RESERVED, NOT_AVAILABLE;
-
 }
 
 public class Movie {
-
 	String movieName;
 	int movieId;
 	int durationInMins;
@@ -83,55 +62,40 @@ public class Movie {
 	Genre genre;
 	Date releaseDate;
 	Map<String, List<Show>> cityShowMap;
-
 }
 
 public enum Genre {
-
 	SCI_FI, DRAMA, ROM_COM, FANTASY;
 }
 
 public class User {
-
 	int userId;
 	Search searchObj;
-
 }
 
 public class SystemMember extends User {
-
 	Account account;
 	String name;
 	String email;
 	Address address;
-
 }
 
-
-
 public class Member extends SystemMember {
-
 	public Booking makeBooking(Booking booking);
 	public List<Booking> getBooking();
-
 }
 
 public class Admin extends SystemMember {
-
 	public boolean addMovie(Movie moivie);
 	public boolean addShow(Show show);
-
 }
 
 public class Account {
-
 	String userName;
 	String password;
-
 }
 
 public class Search {
-
 	public List<Movie> searchMoviesByNames(String name);
 	public List<Movie> searchMoviesByGenre(Genre genre);
 	public List<Movie> searchMoviesByLanguage(String language);
@@ -139,7 +103,6 @@ public class Search {
 }
 
 public class Booking {
-
 	String bookingId;
 	Date bookingDate;
 	Member member;
@@ -149,28 +112,21 @@ public class Booking {
 	double totalAmount;
 	List<Seat> seats;
 	Payment paymentObj;
-
 	public boolean makePayment(Payment payment);
-
 }
 
 public class Payment {
-
 	double amount;
 	Date paymentDate;
 	int transactionId;
 	PaymentStatus paymentStatus;
-
 }
 
 public enum BookingStatus {
-
 	REQUESTED, PENDING, CONFIRMED, CANCELLED;
 }
 
 public enum PaymentStatus {
-
 	UNPAID, PENDING, COMPLETED, DECLINED, CANCELLED, REFUNDED;
-
 }
 
--- system-design/files/ParkingLot.java
+++ system-design/files/ParkingLot.java
@@ -1,49 +1,34 @@
 class ParkingLot {
-	
 	List<ParkingFloor> parkingFloors;
 	List<Entrance> entrances;
 	List<Exit> exits;
-
 	Address address; 
-
 	String parkingLotName;
-
 	public boolean isParkingSpaceAvailableForVehicle(Vehicle vehicle);
 	public boolean updateParkingAttndant(ParkingAttendant parkingAttendant, int gateId)
-
 }
 
 class ParkingFloor {
-	
 	int levelId;
 	List<ParkingSpace> parkingSpaces;
-
 	ParkingDisplayBoard parkingDisplayBoard;
-
 }
 
 class Gate {
-
 	int gateId;
 	ParkingAttendant parkingAttendant;
-
 }
 
 class Entrance extends Gate {
-
 	public ParkingTicket getParkingTicket(Vehicle vehicle);
-
 }
 
 class Exit extends Gate {
-
 	public ParkingTicket payForParking(ParkingTicket parkingTicket, PaymentType paymentType);
-
 }
 
 
 class Address {
-
 	String country;
 	String state;
 	String city;
@@ -52,26 +37,20 @@ class Address {
 }
 
 class ParkingSpace {
-	
 	int spaceId;
 	boolean isFree;
 	double costPerHour;
 	Vehicle vehicle;
 	ParkingSpaceType parkingSpaceType;
-
 }
 
 class ParkingDisplayBoard {
-
 	Map<ParkingSpaceType, Integer> freeSpotsAvailableMap;
-
 	public void updateFreeSpotsAvailable(ParkingSpaceType parkingSpaceType, int spaces);
-
 }
 
 
 class Account {
-
 	String name;
 	String email;
 	String password;
@@ -81,36 +60,26 @@ class Account {
 }
 
 class Admin extends Account {
-
 	public boolean addParkingFloor(ParkingLot parkingLot, ParkingFloor floor);
 	public boolean addParkingSpace(ParkingFloor floor, ParkingSpace parkingSpace);
 	public boolean addParkingDisplayBoard(ParkingFloor floor, ParkingDisplayBoard parkingDisplayBoard);
-	.
-	.
-	.
 
 }
 
 class ParkingAttendant extends Account {
-
 	Payment paymentService;
-
 	public boolean processVehicleEntry(Vehicle vehicle);
 	public PaymentInfo processPayment(ParkingTicket parkingTicket, PaymentType paymentType);
-
 }
 
 class Vehicle {
-	
 	String licenseNumber;
 	VehicleType vehicleType;
 	ParkingTicket parkingTicket;
 	PaymentInfo paymentInfo;
-
 }
 
 class ParkingTicket {
-	
 	int ticketId;
 	int levelId;
 	int spaceId;
@@ -119,50 +88,38 @@ class ParkingTicket {
 	ParkingSpaceType parkingSpaceType;
 	double totalCost;
 	ParkingTicketStatus parkingTicketStatus;
-
 	public void updateTotalCost();
 	public void updateVehicleExitTime(Date vehicleExitDateTime);
-
 } 
 
 public enum PaymentType {
-
 	CASH, CEDIT_CARD, DEBIT_CARD, UPI;
 }
 
 public enum ParkingSpaceType {
-	
 	BIKE_PARKING, CAR_PARKING, TRUCK_PARKING
-
 }
 
 class Payment {
-
 	public PaymentInfo makePayment(ParkingTicket parkingTicket, PaymentType paymentType);
 }
 
 public class PaymentInfo {
-
 	double amount;
 	Date paymentDate;
 	int transactionId;
 	ParkingTicket parkingTicket;
 	PaymentStatus paymentStatus;
-
 }
 
 public enum VehicleType {
-	
 	BIKE, CAR, TRUCK;
 }
 
 public enum ParkingTicketStatus {
-	
 	PAID, ACTIVE;
 }
 
 public enum PaymentStatus {
-
 	UNPAID, PENDING, COMPLETED, DECLINED, CANCELLED, REFUNDED;
-
 }

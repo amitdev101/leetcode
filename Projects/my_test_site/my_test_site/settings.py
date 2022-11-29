2970174c676325db67e2da861d7edf0f18c3f211
Amit <fulldev101@gmail.com>
2022-11-29 10:45:46 +0530

    secret key changed

--- Projects/my_test_site/my_test_site/settings.py
+++ Projects/my_test_site/my_test_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&3ukm8^nz82lpfsasf_ucxm^i)%o$u6&3tu=bf*^vf%8hk2xq'
+SECRET_KEY = 'YOUR_SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

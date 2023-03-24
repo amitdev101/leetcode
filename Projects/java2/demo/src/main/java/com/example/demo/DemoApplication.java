d5c726f46dc58b025800abbdad2a8a6b8082687b
Amit <fulldev101@gmail.com>
2023-03-24 23:49:20 +0530

    modified .gitignore and added contents

---
+++ Projects/java2/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping
+	public List<String> hello(){
+		List<String> mylist = new ArrayList<>();
+		mylist.add("hello 1");
+		mylist.add("hello 2");
+		return mylist;
+	}
+}
